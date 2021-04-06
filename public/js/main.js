1e93dd6b59fecedfd4473b95465d6336d749b92f
00009985 <00009985wiut@gmail.com>
2021-04-06 08:31:41 +0500

    added some code for connection

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,18 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
 
 
 socket.on('message', message =>{
     console.log(message);
 });
+
+//Submitting the message 
+chatForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const msg = e.target.elements.msg.value;
+
+    //Send message to server
+    socket.emit('chatMessage', msg);
+})
