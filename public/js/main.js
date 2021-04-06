78ba0162183c9b543e70043f7257a1f151c323b3
00009985 <00009985wiut@gmail.com>
2021-04-06 10:21:26 +0500

    added code

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,15 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
-
+//outputing the message from server
 socket.on('message', message =>{
     console.log(message);
+    outputMessage(message);
+
+    //moving down when the new message is sent
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 //Submitting the message 
@@ -15,4 +20,19 @@ chatForm.addEventListener('submit', (e) =>{
 
     //Send message to server
     socket.emit('chatMessage', msg);
+
+    //clearing the input box if the message was sent
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus(); 
 })
+
+//output the message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML= `<p class="meta"> Brad <span>9:12pm</span></p>
+    <p class="text">
+    ${message}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
